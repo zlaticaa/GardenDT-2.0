2ecb2462bd38d540c26af71bd3e4a003e00b708c
Marceline <nyaccoon@gmail.com>
2026-06-24 15:38:08 +0200

    preparing the agent

--- Assets/Scripts/ReinforcementLearning/GardenAgent.cs
+++ Assets/Scripts/ReinforcementLearning/GardenAgent.cs
@@ -9,15 +9,36 @@ using UnityEngine;
 
 public class GardenAgent : Agent
 {
-    private int observationsCounter = 0;
     private PillarMath mathComponent;
     private List<Vector3> allBuildings = new();
+    private List<int> allBuildingIds = new();
     private List<Vector3> allFloors = new();
+    private List<int> allFloorIds = new();
+    [SerializeField] private List<BuildingData> buildings = new();
+    [SerializeField] private List<FloorData> floors = new();
     [SerializeField] private float scoreBase = 10;
     [SerializeField] private float pillar1Mult;
     [SerializeField] private float pillar2Mult;
     [SerializeField] private float pillar3Mult;
     [SerializeField] private float pillar4Mult;
+
+    private int steps;
+    private const int MAX_STEPS = 300;
+    private const int totalTiles = 3 * 3;
+
+    public void Start()
+    {
+        steps = 0;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        steps = 0;
+        allBuildings.Clear();
+        allFloors.Clear();
+        //add reload floor/grid
+        mathComponent.Recalculate();
+    }
 
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -26,8 +47,21 @@ public class GardenAgent : Agent
         sensor.AddObservation(mathComponent.Pillar2Score);
         sensor.AddObservation(mathComponent.Pillar3Score);
         sensor.AddObservation(mathComponent.Pillar4Score);
-        
-        
+
+        for (int i = 0; i < totalTiles; i++)
+        {
+            if (allBuildingIds.Count <= i && allBuildings.Count <= i)
+            {
+                sensor.AddObservation(allBuildingIds[i]);
+                sensor.AddObservation(allBuildings[i]);
+            }
+
+            if (allFloors.Count <= i && allFloors.Count <= i)
+            {
+                sensor.AddObservation(allFloors[i]);
+                sensor.AddObservation(allFloorIds[i]);
+            }
+        }
         
     }
 
@@ -48,6 +82,10 @@ public class GardenAgent : Agent
             foreach(Building building in tempList)
             {
                 allBuildings.Add(building.gameObject.transform.position);
+                for (int i = 0; i < buildings.Count; i++)
+                {
+                   if(building.GetData() == buildings[i]) allBuildingIds.Add(i);
+                }
             }
         }
         else
@@ -58,9 +96,13 @@ public class GardenAgent : Agent
         if (mathComponent.GetAllFloors() != null)
         {
             List<FloorBuilding> tempList = mathComponent.GetAllFloors();
-            foreach (FloorBuilding floorBuilding in tempList)
+            foreach (FloorBuilding floor in tempList)
             {
-                allFloors.Add(floorBuilding.gameObject.transform.position);
+                allFloors.Add(floor.gameObject.transform.position);
+                for (int i = 0; i < floors.Count; i++)
+                {
+                    if(floor.GetData() == floors[i]) allFloorIds.Add(i);
+                }
             }
         }
         else
